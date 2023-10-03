2f0ed5b67234cd3eb8fbbdd6be6e117d581d88ea
Ben Manley <benjamintmanley@gmail.com>
2023-10-03 18:31:17 -0500

    add uuid package

--- src/pages/NewCar/NewCar.jsx
+++ src/pages/NewCar/NewCar.jsx
@@ -1,6 +1,16 @@
 import './NewCar.css'
+import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 
 const NewCar = (props) => {
+  const [formData, setFormData] = useState({
+    _id: uuidv4().replaceAll('-', ''),
+    make: '',
+    model: '',
+    year: '',
+    color: ''
+  })
+
   return (
     <>
       <div className="new-car-container">
@@ -8,6 +18,9 @@ const NewCar = (props) => {
         <form>
           <input type="text" name="make" placeholder="Make"/>
           <input type="text" name="model" placeholder="Model"/>
+          <input type="text" name="year" placeholder="Year"/>
+          <input type="text" name="color" placeholder="Color"/>
+          <button type="submit">Add Car 🏎️</button>
         </form>
       </div>
     </>
